c61e0a68250196c0ac82c6d8e68f80b811858ef3
mateusLimadeMacedo <mateuslimamacedo2008@gmail.com>
2026-04-13 09:00:24 -0300

    feat: criando a api de livro

--- core/views/__init__.py
+++ core/views/__init__.py
@@ -3,4 +3,5 @@ from .user import UserRegistrationView, UserViewSet
 from .categoria import CategoriaViewSet
 from .editora import EditoraViewSet
 from .autor import AutorViewSet
+from .livro import LivroViewSet
 
