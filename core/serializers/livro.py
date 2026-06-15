7ced98e55f4d67721d5112d5455e64a9d6a98a7f
renan1811 <rennlima477@gmail.com>
2026-06-14 21:39:06 -0300

    fix; arrumando erros para o deploy

--- core/serializers/livro.py
+++ core/serializers/livro.py
@@ -1,26 +1,37 @@
-
-from rest_framework.serializers import ModelSerializer, SlugRelatedField
+from rest_framework.serializers import (
+    ModelSerializer,
+    SlugRelatedField,
+    ListField,
+    CharField,
+)
 
 from uploader.models import Image
 from uploader.serializers import ImageSerializer
 
 from core.serializers.autor import AutorSerializer
+from core.models import Livro, Autor
 
-from core.models import Livro
+
 class LivroRetrieveSerializer(ModelSerializer):
     capa = ImageSerializer(required=False)
 
     class Meta:
         model = Livro
-        fields = '__all__'
+        fields = "__all__"
         depth = 1
+
 
 class LivroSerializer(ModelSerializer):
     autores = AutorSerializer(many=True, read_only=True)
+    autores_nomes = ListField(
+        child=CharField(max_length=50),
+        write_only=True,
+        required=False,
+    )
     capa_attachment_key = SlugRelatedField(
-        source='capa',
+        source="capa",
         queryset=Image.objects.all(),
-        slug_field='attachment_key',
+        slug_field="attachment_key",
         required=False,
         write_only=True,
     )
@@ -28,4 +39,29 @@ class LivroSerializer(ModelSerializer):
 
     class Meta:
         model = Livro
-        fields = '__all__'
+        fields = "__all__"
+
+    def create(self, validated_data):
+        autores_nomes = validated_data.pop("autores_nomes", [])
+        livro = Livro.objects.create(**validated_data)
+        self._set_autores(livro, autores_nomes)
+        return livro
+
+    def update(self, instance, validated_data):
+        autores_nomes = validated_data.pop("autores_nomes", None)
+        livro = super().update(instance, validated_data)
+        if autores_nomes is not None:
+            self._set_autores(livro, autores_nomes)
+        return livro
+
+    def _set_autores(self, livro, autores_nomes):
+        if not autores_nomes:
+            return
+        autores = []
+        for nome in autores_nomes:
+            nome = nome.strip()
+            if not nome:
+                continue
+            autor, _ = Autor.objects.get_or_create(nome=nome)
+            autores.append(autor)
+        livro.autores.set(autores)
